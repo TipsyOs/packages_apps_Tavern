a646428c9c259df597c768b87705a095300a099c
theringmaster <gzroms@hotmail.com>
2016-11-28 11:34:48 -0500

    Repair derp

--- src/com/gzr/wolvesden/fragments/Recents.java
+++ src/com/gzr/wolvesden/fragments/Recents.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.gzr.dutweaks.wolvesden;
+package com.gzr.wolvesden.fragments;
 
 import android.content.Context;
 import android.content.ContentResolver;
