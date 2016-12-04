d3671ad91708ec15d4362a4b0a2bc884e3270e14
Mazda-- <mazdarider23@gmail.com>
2016-12-04 08:39:51 -0500

    Remove/Disable OmniSwitch PS and features if package is uninstalled

--- src/com/gzr/wolvesden/fragments/Recents.java
+++ src/com/gzr/wolvesden/fragments/Recents.java
@@ -34,6 +34,7 @@ import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
 import com.android.settings.R;
+import com.android.internal.util.validus.ValidusUtils;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -151,6 +152,9 @@ public class Recents extends SettingsPreferenceFragment implements OnPreferenceC
         if (omniRecents) {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(false);
+        } else if (!ValidusUtils.isPackageInstalled(getActivity(), OMNISWITCH_PACKAGE_NAME)) {
+            mOmniRecents.setEnabled(false);
+            mStockRecents.setEnabled(true);
         } else {
             mOmniRecents.setEnabled(true);
             mStockRecents.setEnabled(true);
