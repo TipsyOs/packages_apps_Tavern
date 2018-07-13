DirtyUnicorns/android_packages_apps_DU-Tweaks

Clean up Accent Picker v2
Change-Id: I4a8a1854170433307ec15624188048f813e5f30e

src/com/dirtyunicorns/tweaks/preferences/CategoryPreference.java
@@ -16,15 +16,13 @@
 
 package com.dirtyunicorns.tweaks.preferences;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.om.IOverlayManager;
-import android.content.om.OverlayInfo;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.graphics.PorterDuff;
-import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.os.UserHandle;
 import android.support.annotation.VisibleForTesting;
 import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.preference.Preference;
@@ -41,6 +39,8 @@ import android.widget.TextView;
 import com.android.settings.R;
 import com.android.settings.Utils;
 
+import com.android.internal.statusbar.ThemeAccentUtils;
+
 public class CategoryPreference extends Preference {
 
     private final View.OnClickListener mClickListener = v -> performClick(v);
@@ -49,12 +49,14 @@ public class CategoryPreference extends Preference {
     private boolean mAllowDividerBelow;
 
     private IOverlayManager mOverlayManager;
+    private int mCurrentUserId;
 
     public CategoryPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mOverlayManager = IOverlayManager.Stub.asInterface(
                 ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mCurrentUserId = ActivityManager.getCurrentUser();
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Preference);
 
@@ -84,17 +86,6 @@ public class CategoryPreference extends Preference {
 
         ImageView imageview = (ImageView) holder.findViewById(android.R.id.icon);
         imageview.getDrawable().setColorFilter(getContext().getResources().getColor(
-                isUsingWhiteAccent() ? R.color.dirty_tweaks_light_category_icon_tint : R.color.dirty_tweaks_dark_category_icon_tint), PorterDuff.Mode.SRC_IN);
-    }
-
-    private boolean isUsingWhiteAccent() {
-        OverlayInfo themeInfo = null;
-        try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.accents.white",
-                    UserHandle.USER_CURRENT);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return themeInfo != null && themeInfo.isEnabled();
+                  ThemeAccentUtils.isUsingWhiteAccent(mOverlayManager, mCurrentUserId) ? R.color.dirty_tweaks_light_category_icon_tint : R.color.dirty_tweaks_dark_category_icon_tint), PorterDuff.Mode.SRC_IN);
     }
 }

TipsyOs/packages_apps_Tavern

src/com/gzr/tavern/preference/CategoryPreference.java
@@ -16,15 +16,13 @@
 
 package com.gzr.tavern.preference;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.om.IOverlayManager;
-import android.content.om.OverlayInfo;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.graphics.PorterDuff;
-import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.os.UserHandle;
 import android.support.annotation.VisibleForTesting;
 import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.preference.Preference;
@@ -41,6 +39,8 @@ import android.widget.TextView;
 import com.android.settings.R;
 import com.android.settings.Utils;
 
+import com.android.internal.statusbar.ThemeAccentUtils;
+
 public class CategoryPreference extends Preference {
 
     private final View.OnClickListener mClickListener = v -> performClick(v);
@@ -49,12 +49,14 @@ public class CategoryPreference extends Preference {
     private boolean mAllowDividerBelow;
 
     private IOverlayManager mOverlayManager;
+    private int mCurrentUserId;
 
     public CategoryPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mOverlayManager = IOverlayManager.Stub.asInterface(
                 ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mCurrentUserId = ActivityManager.getCurrentUser();
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Preference);
 
@@ -84,17 +86,6 @@ public class CategoryPreference extends Preference {
 
         ImageView imageview = (ImageView) holder.findViewById(android.R.id.icon);
         imageview.getDrawable().setColorFilter(getContext().getResources().getColor(
-                isUsingWhiteAccent() ? R.color.BottomBarBackgroundColor : android.R.color.white), PorterDuff.Mode.SRC_IN);
-    }
-
-    private boolean isUsingWhiteAccent() {
-        OverlayInfo themeInfo = null;
-        try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.accents.white",
-                    UserHandle.USER_CURRENT);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return themeInfo != null && themeInfo.isEnabled();
+                  ThemeAccentUtils.isUsingWhiteAccent(mOverlayManager, mCurrentUserId) ? R.color.tavern_light_category_icon_tint : R.color.tavern_dark_category_icon_tint), PorterDuff.Mode.SRC_IN);
     }
 }